67cc756a31364a0d993e431c9fbdacc5c78f7cad
mizzz（ずーみー）/いゔる。 <mizzz._.dev@outlook.jp>
2026-08-10 10:24:04 +0900

    既存TypeScript Challengeのhidden test importを修正

--- problems/examples/ts-feature-user-display/tests/hidden/user.hidden.test.ts
+++ problems/examples/ts-feature-user-display/tests/hidden/user.hidden.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert/strict";
 import test from "node:test";
-import { formatDisplayName } from "../../starter/user";
+import { formatDisplayName } from "../../starter/user.ts";
 
 test("uses nickname when present", () => {
   assert.equal(formatDisplayName({ firstName: "Hanako", lastName: "Sato", nickName: "hana" }), "hana");
